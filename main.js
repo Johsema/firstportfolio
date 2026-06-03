4aa8d0bcae965e4cb484285f43be696f1fe8cc80
Johsema <josemariasuarez1698@gmail.com>
2026-06-03 15:59:58 -0400

    IMPLEMENTANDO FONDO Y DANDO ESTILOS A LA PAGINA PRINCIPAL

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ const menuBtn = document.querySelector('.menu-nav')
 const navLista = document.querySelector('.container-nav_lista')
 
 menuBtn.addEventListener('click',()=>{
+
     navLista.classList.toggle('isActive')
+    // Verificamos el texto actual del botón para alternarlo
+    if (menuBtn.textContent === '=') {
+        menuBtn.textContent = 'x';
+    } else {
+        menuBtn.textContent = '=';
+    }
 })
